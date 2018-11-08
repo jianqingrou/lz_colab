4fa17263955a73f8dcdf1d111e2ac67616b76790
lz-google <44837570+lz-google@users.noreply.github.com>
2018-11-08 09:38:11 +0800

    Update lz.bash

--- lz.bash
+++ lz.bash
@@ -6,7 +6,8 @@ wget https://docs.google.com/uc?id=1iXR6TkobaNjjMWYo6GA7EtdWhoa0GTlJ -O autogtp
 wget https://docs.google.com/uc?id=160bMTLjGEJJnYIJAQSrH3Gh2_pXDR8it -O leelaz_opencl_tuning
 gdown --id '1a8Z4mBhGV100sBcK031QMd-7vvW2GObA' -O febf1d9c230f9c6f8a7b83056faa87779abb09ae64b538b836f99c91af50b0b8.gz
 mkdir networks && mv *.gz networks
-# mv leelaz_opencl_tuning ~/.local/share/leela-zero
+mkdir ~/.local/share/leela-zero
+mv leelaz_opencl_tuning  ~/.local/share/leela-zero
 chmod +x leelaz
 chmod +x autogtp
 ./autogtp -k sgf
