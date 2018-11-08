869790af5d1581769063d6b2611c2f7d1114780c
lz-google <44837570+lz-google@users.noreply.github.com>
2018-11-08 09:29:09 +0800

    Update lz.bash

--- lz.bash
+++ lz.bash
@@ -1,4 +1,4 @@
-apt install libboost-program-options-dev libqt5core5a
+apt install libboost-program-options-dev libboost-filesystem-dev libqt5core5a
 pip install gdown
 wget https://docs.google.com/uc?id=1jyN9nJhkKP1jxXJvXVFCDMItLc8IX6cF -O validation
 wget https://docs.google.com/uc?id=1fOPOEJvqu1mRql5BzOKJtZpOlWlOmX3E -O leelaz
@@ -7,5 +7,6 @@ wget https://docs.google.com/uc?id=160bMTLjGEJJnYIJAQSrH3Gh2_pXDR8it -O leelaz_o
 gdown --id '1a8Z4mBhGV100sBcK031QMd-7vvW2GObA' -O febf1d9c230f9c6f8a7b83056faa87779abb09ae64b538b836f99c91af50b0b8.gz
 mkdir networks && mv *.gz networks
 # mv leelaz_opencl_tuning ~/.local/share/leela-zero
-chmod +x *
+chmod +x leelaz
+chmod +x autogtp
 ./autogtp -k sgf
