3250ab89ab94f9f54e9feaa82870fc3edb010e93
lz-google <za.gozero.engine@gmail.com>
2018-11-15 19:18:27 +0800

    update lz.bash

--- lz.bash
+++ lz.bash
@@ -1,5 +1,12 @@
-apt install libboost-program-options-dev libboost-filesystem-dev libqt5core5a
 pip install gdown
+
+gdown --id 14YqdU62OmFtV2QbqRDpgAoTzshdINKn2
+gdown --id 1U-WzLiWeUcxBKltnWFNGVEry_ZZyvcIg
+gdown --id 1om3JPX9v8r3gndNL0G8x5RtQCl5Q0cE6
+gdown --id 1F02n0hnHoTE0qtB6RBYp9hL0T4vhRE_Y
+gdown --id 1mDJgTTSzBNVbYdW__JEgr5ISQmfoZvd-
+mv *.so.* /usr/lib/x86_64-linux-gnu
+
 wget https://docs.google.com/uc?id=1jyN9nJhkKP1jxXJvXVFCDMItLc8IX6cF -O validation
 wget https://docs.google.com/uc?id=1fOPOEJvqu1mRql5BzOKJtZpOlWlOmX3E -O leelaz
 wget https://docs.google.com/uc?id=1iXR6TkobaNjjMWYo6GA7EtdWhoa0GTlJ -O autogtp
