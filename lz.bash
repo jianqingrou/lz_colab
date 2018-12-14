431c0dad7b70199a5f3ef582e33cd0db97f3b14a
lz-google <za.gozero.engine@gmail.com>
2018-12-14 15:00:50 +0800

    update lz.bash

--- lz.bash
+++ lz.bash
@@ -4,7 +4,7 @@ gdown --id 1d6ZWhyLOVEh3WqcotjwTtu_bNGef3Ksl
 tar zxf lz0.16_colab.tgz
 mv *.so.* /usr/lib/x86_64-linux-gnu
 
-gdown --id '11jr8jDePnslkdgRuWGxBaOC-GN81xMIj' -O '8ef6a92408fadf7330556aa05e1575851672d6b46e12c52242d2a84996ccb7c7.gz'
+gdown --id '1xWond1miB8IdvlOSBHGiilWGaWuuhugQ' -O '8b28fe3c8df08d879dc70cbba9a8d2d3e56f4dce6cbab2e1e289851f24044288.gz'
 mkdir networks && mv *.gz networks
 mkdir ~/.local/share/leela-zero
 mv leelaz_opencl_tuning  ~/.local/share/leela-zero
