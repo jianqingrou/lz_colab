1a45b22e78e0fa5d5aceb96906983da6eac9d31f
lz-google <za.gozero.engine@gmail.com>
2019-04-02 19:09:40 +0800

    update lz.bash

--- lz.bash
+++ lz.bash
@@ -4,8 +4,8 @@ gdown --id 1d6ZWhyLOVEh3WqcotjwTtu_bNGef3Ksl
 tar zxf lz0.16_colab.tgz
 mv *.so.* /usr/lib/x86_64-linux-gnu
 
-echo -e "\n==========从google drive下载 "215"号 权重=========="
-gdown --id '1zuL76BjVt-Qgc6zTPoJVFMssZhxjJhUe' -O '358242226f18fe449d38557866a51cc612f912af1bcf261ad36d613ec57f21aa.gz'
+echo -e "\n==========从google drive下载 "216"号 权重=========="
+gdown --id '1m9bNUiWB6Bbat7ft6XITSQJoL_7Zck5R' -O '7a0a5bcdd0574366cb68ab1e6139ca9f625feeed546957ca59c50dad35a394f6.gz'
 mkdir networks && mv *.gz networks
 mkdir ~/.local/share/leela-zero
 mv leelaz_opencl_tuning  ~/.local/share/leela-zero
